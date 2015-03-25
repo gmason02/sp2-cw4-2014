28c89e7b2f0efc5ac2839cbcaa2f9f56ef002077
gmason02 <gerard.mason@hackney.gov.uk>
2015-03-25 20:22:42 +0000

    Added as set out by spec

---
+++ BattleShipGame.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class BattleShipGame {
+	
+	Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args){
+		
+		//Create a new Ocean from Ocean class
+		//Have Ocean fill with random placed ships
+		//Display Ocean. Should all of these be called from Ocean class?
+	}
+	
+}
