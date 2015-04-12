65750cd0f06364ccfaa32071f99e4f60e48b9c5f
gmason02 <gerard.mason@hackney.gov.uk>
2015-04-12 23:09:59 +0100

    Updated

--- Ocean.java
+++ Ocean.java
@@ -2,21 +2,84 @@
 public class Ocean {
 	
 	Ship[][] ships = new Ship[10][10];
-	int shotsFired = 0;
-	int hitCount = 0;
-	public int shipsSunk =0;
-	
+	int shotsFired;
+	int hitCount;
+	int shipsSunk;
+	final int totalShips = 10;
+
 	Ocean(){
-		//initialising variables as per instructions. Initialisation above redundant REMOVE
+		
 		shotsFired = 0;
 		hitCount = 0;
 		shipsSunk = 0;
 		
 		//i is rows and j is columns.
 		for(int i = 0; i<ships.length; i++){
-			for(int j = 0; j<ships.length; j++){
+			for(int j = 0; j<ships[i].length; j++){
 				ships[i][j] = new EmptySea();
 			}
+		}		
+	}
+	
+	void placeAllShipsRandomly(){
+		
+	}
+	
+	boolean isOccupied(int row, int column){
+		
+	}
+	
+	boolean shootAt(int row, int column){
+		
+		
+	}
+	
+	
+	int getShotsFired(){
+		return shotsFired;
+	}
+	
+	int getHitCount(){
+		return hitCount;
+	}
+	
+	int getShipsSunk(){
+		return shipsSunk;
+	}
+	
+	boolean isGameOver(){
+		//int numSunk = getShipsSunk();
+		if(getShipsSunk() == totalShips){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+		
+	}
+	
+	Ship[][] getShipArray(){
+		return ships;
+	}
+	
+	void print(){
+
+		System.out.print("  ");
+		
+		for(int i = 0; i < ships.length; i++){
+			System.out.print(i + " ");
+		}
+		
+		System.out.println();
+		
+		for(int j = 0; j < ships.length; j++){
+			System.out.print(j+" "); 
+			
+			for(int k=0; k<ships[0].length; k++){
+				System.out.print("."+" ");
+			}
+			System.out.println("");
 		}
 	}
 }
